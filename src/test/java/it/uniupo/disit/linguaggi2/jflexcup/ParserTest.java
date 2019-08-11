b1966a289ffe32cab8bf274d8451f2cf443cf01e
Alessandro Borsoi <alessandro.borsoi1984@gmail.com>
2019-08-11 14:21:11 +0200

    Other tests

--- src/test/java/it/uniupo/disit/linguaggi2/jflexcup/ParserTest.java
+++ src/test/java/it/uniupo/disit/linguaggi2/jflexcup/ParserTest.java
@@ -22,11 +22,43 @@ class ParserTest {
     }
 
     @Test
-    void parseNotDeclaredId() throws Exception {
+    void parseNotDeclaredIdOnAssign() throws Exception {
         Scanner scanner = new Scanner(new StringReader("a = 5"));
         Parser parser = new Parser(scanner);
         parser.parse();
         assertEquals("id: 'a' never declared\n", outContent.toString());
+    }
+
+    @Test
+    void parseNotDeclaredIdOnPrint() throws Exception {
+        Scanner scanner = new Scanner(new StringReader("print foo"));
+        Parser parser = new Parser(scanner);
+        parser.parse();
+        assertEquals("id: 'foo' never declared\n", outContent.toString());
+    }
+
+    @Test
+    void parseInvalidTypesProgram() throws Exception {
+        Scanner scanner = new Scanner(new StringReader("int b\n" +
+                "int a\n" +
+                "a = 5.2\n" +
+                "b = a + 3.2\n" +
+                "print b\n"));
+        Parser parser = new Parser(scanner);
+        parser.parse();
+        assertEquals("type INT of 'a' not compatible with type FLOAT\n" +
+                "type INT of 'b' not compatible with type FLOAT\n", outContent.toString());
+    }
+
+    @Test
+    void parseNotConvertibleType() throws Exception {
+        Scanner scanner = new Scanner(new StringReader("int foo\n" +
+                "float bar\n" +
+                "bar = 1.5\n" +
+                "foo = bar"));
+        Parser parser = new Parser(scanner);
+        parser.parse();
+        assertEquals("type INT of 'foo' not compatible with type FLOAT\n", outContent.toString());
     }
 
     @Test
@@ -41,16 +73,4 @@ class ParserTest {
         assertEquals("", outContent.toString());
     }
 
-    @Test
-    void parseInvalidTypesProgram() throws Exception {
-        Scanner scanner = new Scanner(new StringReader("int b\n" +
-                "int a\n" +
-                "a = 5.2\n" +
-                "b = a + 3.2\n" +
-                "print b\n"));
-        Parser parser = new Parser(scanner);
-        parser.parse();
-        assertEquals("type INT not compatible with FLOAT\n" +
-                "type INT not compatible with FLOAT\n", outContent.toString());
-    }
 }
